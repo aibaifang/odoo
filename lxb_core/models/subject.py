0110395b40b8b0d43bfde06e6afc24ce388f9907
aibaifang <2543323095@qq.com>
2023-11-27 22:45:44 +0800

    第三次修改

--- lxb_core/models/subject.py
+++ lxb_core/models/subject.py
@@ -2,3 +2,14 @@ from odoo import models, fields, api
 class LxbSubject(models.Model):
   _name = 'lxb.subject'
   _rec_name = 'name'
+  _description = 'LXB Subject'
+  
+  name = fields.Char('科目名称', size=128, required=True)
+  code = fields.Char('科目编码', size=256, required=True)
+  tyep = fields.Selection([('theory', '理论'), ('practical', '实践')
+                          ,('both', '理论实践结合'), ('other','其他')], 
+                          string='类别', default='theory', required=True)
+  subject_type = fields.Selection([('compulsory', '必修'), ('elective', '选修'),
+                                    ], string='科⽬类型', default='compulsory', required=True)
+  _sql_constraints = [('unique_subject_code','unique(code)','科目编码必须唯一！')]
+  
---
+++ lxb_core/models/tcacher.py
@@ -0,0 +1,32 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+
+class LxbTeacher(models.Model):
+    _name = "lxb.teacher"
+    _description = "LXB Teacher"
+    _inherits = {"res.partner": "partner_id"}
+
+    partner_id = fields.Many2one('res.partner', required=True, ondelete='cascade', string='合作伙伴')
+    birth_date = fields.Date(string='出生日期', required=True)
+    gender = fields.Selection([('male', '男'), ('female', '女')], string='性别', required=True)
+    nationality = fields.Many2one('res.country', string='国籍')
+    emergency_contact = fields.Many2one('res.partner', string='紧急联系人')
+    visa_info = fields.Char(string='银行卡', size=64)
+    id_number = fields.Char(string='ID卡号', size=64)
+    login = fields.Char(string='登录', related='partner_id.user_id.login', readonly=1)
+    last_login = fields.Datetime(string='最后登录', related='partner_id.user_id.login_date', readonly=1)
+    emp_id = fields.Many2one('hr.employee', string='员工',)
+    company_id = fields.Many2one('res.company', string='公司', default=lambda self: self.env.user.company_id)
+    teacher_subject_ids = fields.Many2many('lxb.teacher.subject', string='可授科目')
+
+    @api.constrains('birth_date')
+    def _check_birth_date(self):
+        for record in self:
+            if record.birth_date > fields.Date.today():
+                raise ValidationError(_('出生日期不能晚于当前日期！'))
+    
+
+
+
+
